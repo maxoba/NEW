b0983818b9b9f795f4c96318bddcfa37096cbdd3
max <maxoba@yahoo.com>
2025-01-30 01:02:00 -0700

    region

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-provider "aws" {}
+provider "aws" {
+    region = "us-west-2"
+}
+ resource "aws_vpc" "test-vpc" {
+    cidr_block = "10.0.0.0/24"
+
+    tags = {
+        Name = "action-test-vpc"
+    }
+ }
